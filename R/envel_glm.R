bc7a9baa22fba60a5ebef066e24041261d3ae33f
Julio Trecenti <julio.trecenti@gmail.com>
2017-05-13 15:17:20 -0300

    envelope para glm

--- R/envel_glm.R
+++ R/envel_glm.R
@@ -1,35 +1,31 @@
-refit_beta <- function(model, sample, rt){
-  refit(model, sample) %>%
-    resid(type = rt) %>%
-    sort()
-}
-
 #' Envelope plots of generalized linear model
 #'
 #' Draws the envelope plot for the residual distributions of a generalized linear model.
+#'
 #' @param fit a model object - currently working for glm
 #' @param B number of bootstrap iterations
-#' @param resid_type type of the residual to be used
 #'
+#' @import ggplot2
 #' @export
-envel_glm <- function(fit, B = 100, resid_type = 'response'){
-
+envel_glm <- function(fit, B = 100) {
   residual_sample <- simulate(fit, nsim = B) %>%
-    map_df(~refit_beta(model = fit, sample = .x, rt = 'response')) %>%
-    mutate(posto = 1:n()) %>%
-    gather(sim, value, -posto) %>%
-    group_by(posto) %>%
-    summarise(residuo_teorico = mean(value),
-              q025 = quantile(value, prob = 0.025),
-              q975 = quantile(value, prob = 0.975)) %>%
-    mutate(residuo_empirico = fit %>% resid(type = resid_type) %>% sort)
+    purrr::map_df(~refit(fit, .x) %>%
+                    resid(type = 'deviance') %>%
+                    sort()) %>%
+    dplyr::mutate(posto = 1:n()) %>%
+    tidyr::gather(sim, value, -posto) %>%
+    dplyr::group_by(posto) %>%
+    dplyr::summarise(residuo_teorico = mean(value),
+                     q025 = quantile(value, prob = 0.025),
+                     q975 = quantile(value, prob = 0.975)) %>%
+    dplyr::mutate(residuo_empirico = fit %>% resid(type = 'deviance') %>% sort())
 
   residual_sample %>%
-    mutate(o_sonho_acabou = (residuo_empirico > q975)|(residuo_empirico < q025)) %>%
+    dplyr::mutate(banda = (residuo_empirico > q975) | (residuo_empirico < q025)) %>%
     ggplot(aes(x = sort(residuo_teorico), y = sort(residuo_teorico),
                ymin = sort(q025), ymax = sort(q975))) +
     geom_point(aes(x = sort(residuo_teorico), y = sort(residuo_empirico),
-                   color = o_sonho_acabou)) +
+                   color = banda)) +
     geom_ribbon(alpha = 0.3) +
     geom_abline(slope = 1, intercept = 0, linetype = 2) +
     theme_bw(15) +
