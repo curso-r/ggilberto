9aa9217dd5dae15459fe2aa8f2a22bcc737e8ef8
Julio Trecenti <julio.trecenti@gmail.com>
2017-05-13 15:17:25 -0300

    refit

--- R/refit.R
+++ R/refit.R
@@ -1,10 +1,10 @@
 #' Refit a model
 #'
 #' Refits a model changing the response variable
+#'
 #' @param model a model object - currently working for lm and glm
 #' @param y new response variable
-#'
-refit <- function(model, y){
+refit <- function(model, y) {
   x <- as.data.frame(stats::model.matrix(model))
   x$`.y` <- y
   stats::update(model, .y ~ ., data = x)
