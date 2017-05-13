4d1f34efb9978e3a62d47a7536d3b6cf10a9089b
Julio Trecenti <julio.trecenti@gmail.com>
2017-05-13 15:17:00 -0300

    diag data uses broom

---
+++ R/diag-data.R
@@ -0,0 +1,20 @@
+#' Create tibble with useful diagnostic measures
+#'
+#' Uses broom::augment and some other useful measures
+#'
+#' @param model
+#'
+#' @export
+diag_data <- function(model) {
+  w <- model$weights
+  if (is.null(w)) w <- 1
+  d_diag <- model %>%
+    broom::augment() %>%
+    dplyr::mutate(.w = 1, .eta = predict(model),
+                  .pearson = resid(model, type = 'pearson'),
+                  .z = .eta + .pearson / .w) %>%
+    tibble::as_tibble()
+  class(d_diag) <- c(ifelse('glm' %in% class(model),  'diag_glm', 'diag_lm'),
+                     class(d_diag))
+  d_diag
+}
