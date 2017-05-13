2e3073a46ea8be63afd205876ce807c2117ed4ff
Julio Trecenti <julio.trecenti@gmail.com>
2017-05-13 18:47:37 -0300

    globalvars

---
+++ R/zzz.R
@@ -0,0 +1,5 @@
+globalVariables(c(".cooksd", ".eta", ".fitted", ".hat", ".pearson",
+                  ".rownames", ".std.resid", ".w", ".z", "banda", "n",
+                  "posto", "predict", "q025", "q975", "quantile", "resid",
+                  "residuo_empirico", "residuo_teorico", "sim", "simulate",
+                  "value"))
